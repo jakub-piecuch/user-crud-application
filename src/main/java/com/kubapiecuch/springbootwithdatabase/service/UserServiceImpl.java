d9ed28abf7967ae2fb91b8dc4539dd642a666d3e
Jakub Piecuch <kuba.piecuch.kp@gmail.com>
2023-03-05 03:21:33 +0100

    added random space

--- src/main/java/com/kubapiecuch/springbootwithdatabase/service/UserServiceImpl.java
+++ src/main/java/com/kubapiecuch/springbootwithdatabase/service/UserServiceImpl.java
@@ -35,6 +35,7 @@ public class UserServiceImpl implements UserService {
         return userRepository.findAll();
     }
 
+
     @Override
     public User getUserById(Long id) {
         Optional<User> existingUserOptional = userRepository.findById(id);
